44a1d05081022333898003d1797e55f81476d70a
MarcinS <ut_3777@zsp3zamosc.pl>
2024-09-11 10:17:37 +0200

    Dodaj pliki projektów.

---
+++ phoneword/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Android.Telephony;
+using Javax.Security.Auth;
+
+namespace phoneword
+{
+    public partial class MainPage : ContentPage
+    }
+string translatedNumber;
+Private void OnTranslate(object sender, EventArgs e)
+    {
+    string enteredNumber = PhoneNumberFormattingTextWatcher.Text;
+translatedNumber = Core.PhonewordTranslator.ToNumber(enteredNumber);
+
+if (!string.IsNullOrEmpty(translatedNumber))
+{
+    CallButton.IsEnabled = true;
+    CallButton.Text = "Call" + translatedNumber;
+}
+else
+{
+    CallButton.IsEnabled = false;
+    CallButton.Text = "Call";
+
+}
+public partial class MainPage : ContentPage
+{
+    async void OnCall(object, System.EventArgs e)
+    {
+        if (await this.DisplayAlert(
+            "Dial a Number," +
+            "Would you like to call"+translatedNumber + "?>",
+            "yes",
+            "no"))
+            {
+            //Todo
+        }
+    }
+}
+
+public MainPage()
+        {
+            InitializeComponent();
+        }
+
+}
---
+++ phoneword/PhonewordTranslator.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Text;
+namespace Core;
+public static class PhonewordTranslator
+{
+    public static string ToNumber(string raw)
+}
+if (string.IsNullOrWhiteSpace(raw))
+    return null;
+    raw = raw.ToUpperInvariant();
+    var newNumber = new StringBuilder();
+foreach (var c in raw)
+    {
+    if("-0123456789".Contains(c))
+    newNumber.Append(c);
+    else
+    return null;
+    }
+    }
+    return newNumber.ToString();
+}
+static bool Contains(this string keyString, char c)
+{
+    return keyString.IndexOf(c) >= 0;
+}
+static int? TranslateToNumber(char c)
+{
+    for (int i = 0; i < digits.Lenght; i++)
+    {
+        if (digits[i].Contains(c))
+            return 2 + i;
+    }
+    return null;
+}
+}
